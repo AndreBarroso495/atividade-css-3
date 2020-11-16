a3ccc4648df2fec2179b39cce280ca6480d38dd1
FIC <FIC@info.local>
2020-11-16 09:38:50 -0300

    Projeto concluido

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace atividade_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor do salário ");
+            int salário = int.Parse(Console.ReadLine());
+
+           
+            if(salário<500){
+                Console.WriteLine("Você tem o direito de reajuste no salário de 30%!");
+
+                
+            }
+            else{
+                Console.WriteLine("Você não tem direito ao aumento!");
+            }
+
+            }
+        }
+    }
+}
